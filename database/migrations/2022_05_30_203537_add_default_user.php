11f900aa56e2a819c08f017133e01612e8b63c21
Gabriel Ramos <gabrielramos791@gmail.com>
2022-05-31 17:03:47 -0300

    change default user

--- database/migrations/2022_05_30_203537_add_default_user.php
+++ database/migrations/2022_05_30_203537_add_default_user.php
@@ -2,27 +2,24 @@
 
 use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\{Hash, Schema};
 
-return new class extends Migration
-{
+return new class () extends Migration {
     public function up(): void
     {
         User::create([
-            'name'      => 'User',
-            'email'     => 'user@ponto.com',
-            'password'  => Hash::make('password'),
+            'name'     => 'User',
+            'email'    => 'admin@admin.com',
+            'password' => Hash::make('password'),
         ]);
     }
 
     public function down()
     {
         User::where([
-            'name'      => 'User',
-            'email'     => 'user@ponto.com',
-            'password'  => Hash::make('password'),
+            'name'     => 'User',
+            'email'    => 'admin@admin.com',
+            'password' => Hash::make('password'),
         ])->delete();
     }
 };
