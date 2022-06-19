6d0cda742375e8a07ec6cadbbf1225c35a547292
Gabriel Ramos <gabrielramos791@gmail.com>
2022-06-19 18:02:28 -0300

    change index to livewire

--- resources/views/livewire/pages/index.blade.php
+++ resources/views/livewire/pages/index.blade.php
@@ -1,9 +1,9 @@
-<x-layouts.app>
+<div class="flex w-full">
     <x-sidebar />
-
+    
     <div class="flex flex-col w-full">
         <x-index-header />
-
+    
         <livewire:display.time-container />
     </div>
-</x-layouts.app>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,16 @@
 <?php
 
 use App\Http\Controllers\LoginController;
-use App\Http\Livewire\{Login, Register};
+use App\Http\Livewire\{Login, Pages, Register};
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth')->group(function () {
-    Route::view('/', 'pages.index')->name('index');
+    Route::get('/', Pages\Index::class)->name('index');
 
     Route::post('logout', [LoginController::class, 'logout'])->name('logout');
 });
 
-Route::get('login', Login::class)->name('login');
-Route::get('register', Register::class)->name('register');
+Route::middleware('guest')->group(function () {
+    Route::get('login', Login::class)->name('login');
+    Route::get('register', Register::class)->name('register');
+});
