3485290b7c557c9481adf201fb26785edf94e1cd
Gabriel Ramos <gabrielramos791@gmail.com>
2022-06-20 19:33:30 -0300

    add weekday to view

--- resources/views/livewire/switch-day.blade.php
+++ resources/views/livewire/switch-day.blade.php
@@ -1,41 +1,47 @@
-<div class="flex items-center gap-1">
-    <button
-        wire:click="previousDate"
-        class="text-blue-300"
-    >
-        <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="h-7"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-        >
-            <path
-                fill-rule="evenodd"
-                d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
-                clip-rule="evenodd"
-            />
-        </svg>
-    </button>
-
-    <span class="bg-blue-200 text-blue-900 rounded-sm px-2 py-1 select-none">
-        {{ $date->format('M jS, Y') }}
+<div class="flex flex-col items-center gap-1">
+    <span class="text-center min-w-[2.8rem] rounded-sm bg-blue-200 text-blue-800 px-1 py-0.5">
+        {{ $date->format('D') }}
     </span>
 
-    <button
-        wire:click="nextDate"
-        class="text-blue-300"
-    >
-        <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="h-7"
-            viewBox="0 0 20 20"
-            fill="currentColor"
+    <div class="flex items-center gap-1">
+        <button
+            wire:click="previousDate"
+            class="text-blue-300"
         >
-            <path
-                fill-rule="evenodd"
-                d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                clip-rule="evenodd"
-            />
-        </svg>
-    </button>
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-7"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+            >
+                <path
+                    fill-rule="evenodd"
+                    d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+                    clip-rule="evenodd"
+                />
+            </svg>
+        </button>
+
+        <span class="bg-blue-200 text-blue-900 rounded-sm px-2 py-1 select-none">
+            {{ $date->format('M jS, Y') }}
+        </span>
+
+        <button
+            wire:click="nextDate"
+            class="text-blue-300"
+        >
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-7"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+            >
+                <path
+                    fill-rule="evenodd"
+                    d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                    clip-rule="evenodd"
+                />
+            </svg>
+        </button>
+    </div>
 </div>
